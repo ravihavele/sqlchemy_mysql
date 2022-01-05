e8991e220df8fa85ced95d33fefc78b8a8701494
ravihavele <ravindra.havele@opcito.com>
2022-01-05 20:10:11 +0530

    second commit

--- settings.py
+++ settings.py
@@ -13,5 +13,9 @@ app = Flask(__name__)
 
 #configure our database
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+mysqlconnector://' + os.getenv("SQLUSER") + ':' + os.getenv("PASSWORD") \
+                                       + '@' + os.getenv("SERVER") +'/'+ os.getenv("DATABASE")
+print(app.config['SQLALCHEMY_DATABASE_URI'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
+
+
